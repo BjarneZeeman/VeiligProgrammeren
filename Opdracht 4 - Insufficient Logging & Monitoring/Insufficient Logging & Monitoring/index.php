d1b013e0cdce0978c59897cbaa5cfe320e01adb5
BjarneZeeman <2106756@talnet.nl>
2020-12-15 15:29:43 +0100

    veilig programmeren

---
+++ Opdracht 4 - Insufficient Logging & Monitoring/Insufficient Logging & Monitoring/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div>
+		<form method="POST" action="login.php">
+			<input type="email" name="email" placeholder="Email" required>
+			<br>
+			<input type="password" name="password" placeholder="Password" required>
+			<br>
+			<button type="submit" class="button">Login</button>
+		</form>
+	</div>
+</body>
+</html>
